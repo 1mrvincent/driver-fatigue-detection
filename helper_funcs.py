ff44c06d075c871b7b282d12083877136332d802
Vincent Enyinwa <enyinwa.vincent@outlook.com>
2023-10-14 02:06:51 +0100

    tested to ensure code still works

--- helper_funcs.py
+++ helper_funcs.py
@@ -1,64 +1,20 @@
 import math
+import os
 import mediapipe as mp
 import cv2
 import numpy as np
-import tensorflow as tf
 
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 
-STATES = ['CLOSE', 'OPEN']
 TEXT_COLOR=(255, 0, 0)
 
-# def _normalized_to_pixel_coordinates(
-#     normalized_x: float, normalized_y: float, image_width: int,
-#     image_height: int) :
-#   """Converts normalized value pair to pixel coordinates."""
-
-#   # Checks if the float value is between 0 and 1.
-#   def is_valid_normalized_value(value: float) -> bool:
-#     return (value > 0 or math.isclose(0, value)) and (value < 1 or
-#                                                       math.isclose(1, value))
-
-#   if not (is_valid_normalized_value(normalized_x) and
-#           is_valid_normalized_value(normalized_y)):
-#     # TODO: Draw coordinates even if it's outside of the image bounds.
-#     return None
-#   x_px = min(math.floor(normalized_x * image_width), image_width - 1)
-#   y_px = min(math.floor(normalized_y * image_height), image_height - 1)
-#   return x_px, y_px
-
-def draw_landmarks_on_image(rgb_image, detection_result, faceDeresult, model):
-  face_landmarks_list = detection_result.face_landmarks
+def draw_landmarks_on_image(rgb_image, faceDeresult):
+  
   annotated_image = np.copy(rgb_image)
-  left_eye_img = cv2.imread('./left.jpg')
-  right_eye_img = cv2.imread('./right.jpg')
+  left_eye_img = cv2.imread('./out/left.jpg')
+  right_eye_img = cv2.imread('./out/right.jpg')
 
-  # Loop through the detected faces to visualize.
-  # for idx in range(len(face_landmarks_list)):
-  #   face_landmarks = face_landmarks_list[idx]
-
-  #   # Draw the face landmarks.
-  #   face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-  #   face_landmarks_proto.landmark.extend([
-  #     landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks
-  #   ])
-
-  #   # solutions.drawing_utils.draw_landmarks(
-  #   #     image=annotated_image,
-  #   #     landmark_list=face_landmarks_proto,
-  #   #     connections=mp.solutions.face_mesh.FACEMESH_LEFT_EYE,
-  #   #     landmark_drawing_spec=None,
-  #   #     connection_drawing_spec=mp.solutions.drawing_styles
-  #   #     .get_default_face_mesh_contours_style())
-    
-  #   # solutions.drawing_utils.draw_landmarks(
-  #   #     image=annotated_image,
-  #   #     landmark_list=face_landmarks_proto,
-  #   #     connections=mp.solutions.face_mesh.FACEMESH_RIGHT_EYE,
-  #   #     landmark_drawing_spec=None,
-  #   #     connection_drawing_spec=mp.solutions.drawing_styles
-  #   #     .get_default_face_mesh_contours_style())
     
    
   height, width, _ = rgb_image.shape
@@ -77,39 +33,38 @@ def draw_landmarks_on_image(rgb_image, detection_result, faceDeresult, model):
 
 
   #   for keypoint in fdetection.keypoints: # mp_face_dection.get_key_point(fdetection.keypoints, mp_face_dection.FaceKeyPoint.RIGHT_EYE):
-      right_eye = fdetection.keypoints[0]
-      left_eye = fdetection.keypoints[1]
+      right_eye = fdetection.keypoints[1]
+      left_eye = fdetection.keypoints[0]
       
       
       
       if right_eye:
-          # keypoint_px = _normalized_to_pixel_coordinates(right_eye.x, right_eye.y, width, height)
           # color, thickness, radius = (0, 255, 0), 2, 2# 
           # cv2.circle(annotated_image, keypoint_px, thickness, color, radius)
           
           origin_x = min(math.floor(right_eye.x * width), width - 1)
           origin_y = min(math.floor(right_eye.y * height), height - 1)
       
-          start_point = origin_x - math.floor(bbox.width/7) , origin_y - math.floor(bbox.height/7)
-          end_point = origin_x + math.floor(bbox.width/7), origin_y + math.floor(bbox.height/7)
+          start_point = origin_x - math.floor(bbox.width/6) , origin_y - math.floor(bbox.height/8)
+          end_point = origin_x + math.floor(bbox.width/6), origin_y + math.floor(bbox.height/8)
           
-          text_start_point = origin_x - math.floor(bbox.width/7) , (origin_y - math.floor(bbox.height/7))-10
+          text_start_point = origin_x - math.floor(bbox.width/6) , (origin_y - math.floor(bbox.height/6))-10
           
-          x = origin_x - math.floor(bbox.width/7)
-          y = origin_y - math.floor(bbox.height/7)
-          xw = origin_x + math.floor(bbox.width/7)
-          yh = origin_y + math.floor(bbox.height/7)
+          x = origin_x - math.floor(bbox.width/6)
+          y = origin_y - math.floor(bbox.height/6)
+          xw = origin_x + math.floor(bbox.width/6)
+          yh = origin_y + math.floor(bbox.height/6)
           
           right_eye_img = annotated_image[y:yh, x:xw]
           try:
-            cv2.imwrite('right.jpg', right_eye_img)
+            cv2.imwrite('./out/right.jpg', right_eye_img)
           except Exception as e:
             print(e)
           
-          right_eye_state = predict_eye_state(model, './right.jpg')
-          print(f'right eye prediction is >>> {STATES[right_eye_state]}')
+          # right_eye_state = predict_eye_state(model, './out/right.jpg')
+          # print(f'right eye prediction is >>> {STATES[right_eye_state]}')
           cv2.rectangle(annotated_image, start_point, end_point, TEXT_COLOR, 2)
-          cv2.putText(annotated_image, STATES[right_eye_state], text_start_point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, TEXT_COLOR, 2)
+          # cv2.putText(annotated_image, STATES[right_eye_state], text_start_point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, TEXT_COLOR, 2)
 
       
       
@@ -120,45 +75,30 @@ def draw_landmarks_on_image(rgb_image, detection_result, faceDeresult, model):
           
           origin_x = min(math.floor(left_eye.x * width), width - 1)
           origin_y = min(math.floor(left_eye.y * height), height - 1)
-          start_point = origin_x - math.floor(bbox.width/7) , origin_y - math.floor(bbox.height/7)
-          end_point = origin_x + math.floor(bbox.width/7), origin_y + math.floor(bbox.height/7)
+          start_point = origin_x - math.floor(bbox.width/6) , origin_y - math.floor(bbox.height/8)
+          end_point = origin_x + math.floor(bbox.width/6), origin_y + math.floor(bbox.height/8)
           
-          text_start_point = origin_x - math.floor(bbox.width/7) , (origin_y - math.floor(bbox.height/7))-10
+          text_start_point = origin_x - math.floor(bbox.width/6) , (origin_y - math.floor(bbox.height/6))-10
       
           
-          x = origin_x - math.floor(bbox.width/7)
-          y = origin_y - math.floor(bbox.height/7)
-          xw = origin_x + math.floor(bbox.width/7)
-          yh = origin_y + math.floor(bbox.height/7)
+          x = origin_x - math.floor(bbox.width/6)
+          y = origin_y - math.floor(bbox.height/6)
+          xw = origin_x + math.floor(bbox.width/6)
+          yh = origin_y + math.floor(bbox.height/6)
           
           left_eye_img = annotated_image[y:yh, x:xw]
           try:
-            cv2.imwrite('left.jpg', left_eye_img)
+            cv2.imwrite('./out/left.jpg', left_eye_img)
           except Exception as e:
             print(e)
           
-          left_eye_state = predict_eye_state(model, './left.jpg')
-          print(f'left eye prediction is >>> {STATES[left_eye_state]}')
+          # left_eye_state = predict_eye_state(model, './out/left.jpg')
+          # print(f'left eye prediction is >>> {STATES[left_eye_state]}')
           cv2.rectangle(annotated_image, start_point, end_point, TEXT_COLOR, 2)
-          cv2.putText(annotated_image, STATES[left_eye_state], text_start_point, cv2.FONT_HERSHEY_SIMPLEX,0.5,TEXT_COLOR, 2)
+          # cv2.putText(annotated_image, STATES[left_eye_state], text_start_point, cv2.FONT_HERSHEY_SIMPLEX,0.5,TEXT_COLOR, 2)
           
-      
-      
 
   return annotated_image, left_eye_img, right_eye_img
 
-def predict_eye_state(model, eye_img_path:str):
-  
-  input_details = model.get_input_details()
-  output_details = model.get_output_details()
-  
-  image = tf.keras.preprocessing.image.load_img(eye_img_path, target_size=(20,20), grayscale=True)
-  x = tf.keras.preprocessing.image.img_to_array(image)
-  x = tf.expand_dims(x, 0)
-  model.set_tensor(input_details[0]['index'], x)
-  model.invoke()
-  prediction = model.get_tensor(output_details[0]['index'])
-  prediction = tf.concat(prediction, axis=0)
-  prediction = tf.argmax(prediction, axis=1)
-  return prediction[0]
+
 
--- testMediapipe.py
+++ testMediapipe.py
@@ -1,14 +1,15 @@
+import os
+import time
 import mediapipe as mp
 import cv2
-from mediapipe import solutions
-from mediapipe.framework.formats import landmark_pb2
-
+from PIL import Image
 import matplotlib.pyplot as plt
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from mediapipe.tasks.python.vision import RunningMode
 from helper_funcs import draw_landmarks_on_image
 import tensorflow as tf
+import numpy as np
 
 
 mp_drawing = mp.solutions.drawing_utils
@@ -24,52 +25,80 @@ cap.set(3, 640)    # width
 cap.set(4, 420)    # height
 cap.set(10, 100)   # brightness
 
+TEXT_COLOR=(255, 0, 0)
 
-model = tf.lite.Interpreter(model_path='./kaggle_best_model__final-2.tflite')
+SLEEP_THRESHOLD = 8
+
+model = tf.lite.Interpreter(model_path='./kaggle_best_model__final_284k.tflite')
 model.allocate_tensors()
 
+STATES = ['CLOSE', 'OPEN']
 
 
-while True:
-    # Read camera frames
-    success, frame = cap.read()
-    if not success:
-        print('Ignoring empty camera frame.')
-        continue    # use break if reading from a video file 
-      
-    print(f'image shape >>> {frame.shape}')
-    h, w, c = frame.shape
+def predict_eye_state(model):
+  
+    input_details = model.get_input_details()
+    output_details = model.get_output_details()
     
-    # flip the video vertically and change the BGR to RGB
-    frame = cv2.flip(frame, 1)
-    print('frame is flipped')
+    left_image = tf.keras.preprocessing.image.load_img('./out/left.jpg', target_size=(20,20), grayscale=True)
+    right_image = tf.keras.preprocessing.image.load_img('./out/right.jpg', target_size=(20,20), grayscale=True)
     
-    base_options = python.BaseOptions(model_asset_path='face_landmarker.task')
-    options = vision.FaceLandmarkerOptions(base_options=base_options,
-                                        output_face_blendshapes=False,
-                                        output_facial_transformation_matrixes=True,
-                                        running_mode=RunningMode.IMAGE,
-                                        num_faces=1)
+    lx = tf.keras.preprocessing.image.img_to_array(left_image)
+    lx = tf.expand_dims(lx, 0)
+    rx = tf.keras.preprocessing.image.img_to_array(right_image)
+    rx = tf.expand_dims(rx, 0)
     
+    model.set_tensor(input_details[0]['index'], lx)
+    model.invoke()
+    lprediction = model.get_tensor(output_details[0]['index'])
+    lprediction = tf.concat(lprediction, axis=0)
+    lprediction = tf.argmax(lprediction, axis=1)
     
+    model.set_tensor(input_details[0]['index'], rx )
+    model.invoke()
+    rprediction = model.get_tensor(output_details[0]['index'])
+    rprediction = tf.concat(rprediction, axis=0)
+    rprediction = tf.argmax(rprediction, axis=1)
+    
+    prediction = '{:.2f}'.format((rprediction[0]+lprediction[0])/2)
+    
+    if float(prediction) >= 0.5:
+        print(f'xxxxxxxxxxxxxxx-==================== open left>> {lprediction} ~~~~~ right >>{rprediction} ~~~~~~~~~~~ {prediction}')
+        return 1
+    else:
+        print(f'xxxxxxxxxxxxxxx-==================== close left >> {lprediction} ~~~~~ right>>{rprediction}~~~~~~~~~~~ {prediction}')
+        return 0
 
-    fbase_options = python.BaseOptions(model_asset_path='blaze_face_short_range.tflite')
-    foptions = vision.FaceDetectorOptions(base_options=fbase_options)
-    fdetector = vision.FaceDetector.create_from_options(foptions)
-    
-    # show webcame frames
-    # cv2.imshow("Webcam", gray_image)
-    
-    landmarker = vision.FaceLandmarker.create_from_options(options)
+try:
         
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    count = 0
+    while True:
+        
+        # Read camera frames
+        success, frame = cap.read()
+        if not success:
+            print('Ignoring empty camera frame.')
+            continue    # use break if reading from a video file 
+   
+        h, w, c = frame.shape
+        
+        # flip the video vertically and change the BGR to RGB
+        frame = cv2.flip(frame, 1)
 
-    result =landmarker.detect(mp_image)
+        fbase_options = python.BaseOptions(model_asset_path='blaze_face_short_range.tflite')
+        foptions = vision.FaceDetectorOptions(base_options=fbase_options)
+        fdetector = vision.FaceDetector.create_from_options(foptions)
+        
+        # show webcame frames
+        # cv2.imshow("Webcam", gray_image)
+            
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+
+        
+        face_detection_result = fdetector.detect(mp_image)
+        
+        annotated_image, left_eye, right_eye = draw_landmarks_on_image(mp_image.numpy_view(), face_detection_result)
     
-    fdetection_result = fdetector.detect(mp_image)
-    
-    annotated_image, left_eye, right_eye = draw_landmarks_on_image(mp_image.numpy_view(), result, fdetection_result, model)
-    try:
                         
          
         # cv2.imshow(mat=cv2.cvtColor(annotated_image, cv2.COLOR_RGB2GRAY), winname='image')  
@@ -79,16 +108,33 @@ while True:
         
         # cv2.imshow(mat=left_eye, winname='left eye')
         # cv2.imshow(mat=right_eye,  winname='right eye')
+    
         
+        prediction = predict_eye_state(model)
+        print(STATES[prediction])
+        if prediction == 1:
+            count = count-1 if count != 0 else 0
+        else:
+            if count > 20:
+                count=0
+            count = count+1
+        
+        print(count)
+        if count >= SLEEP_THRESHOLD :
+            cv2.putText(annotated_image, "Sleep detected", (20, 20), cv2.FONT_HERSHEY_SIMPLEX,0.5,TEXT_COLOR, 2)
+            
         cv2.imshow(mat=annotated_image, winname='image') 
+       
         
         
         # press q to exit  
         if cv2.waitKey(1) & 0xFF == ord('q'):
           cv2.destroyAllWindows()
           break
-    except Exception as e :
-        print(e)
+        
+        time.sleep(1/4)
+except Exception as e :
+    print(e)
     
 
 # Finally release back the camera resources 
